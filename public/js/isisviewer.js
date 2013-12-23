4780d0e865399d832845ae00ed747c557a6bcd70
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2013-12-23 13:19:31 -0500

    frame selector

--- public/js/isisviewer.js
+++ public/js/isisviewer.js
@@ -1,7 +1,38 @@
 $(document).on("pageinit", function() {
+    // initialize selected frame
+    var selectedframe = "frame1";
+    $("#"+selectedframe).addClass("frameborder");
+    
+    //disable image drag and drop
+    $('.imageframe, .minidiv').on('dragstart', function(event) {
+        event.preventDefault();
+    });
+    
+    //custom scrollbar
     $(".minilist").niceScroll();
     $(".imageframe").niceScroll();
-    $(".imageframe img").css({'max-height':'100%','max-width':'100%'});
-    $(".imageframe img").removeAttr('style');
-    $(".imageframe img").css({'height':'150%'});
+    
+    //set the zomm level
+    $(".imageframe img").css({
+        'max-height': '100%',
+        'max-width': '100%'
+    });
+    //set a different zoom level
+    // $(".imageframe img").removeAttr('style');
+    // $(".imageframe img").css({'height':'400%'});
+    
+    //Click on image frame
+    $(".imageframe").on("mousedown", function() {
+        $(".imageframe").removeClass("frameborder");
+        $(this).addClass("frameborder");
+        selectedframe = $(this).attr('id');
+    });
+    
+    // Click on minidiv
+    $(".minidiv").on("click", function(){
+        var selectedimage;
+        selectedimage = $(this).children("img").attr("src");
+        console.log(selectedimage)
+        $("#"+selectedframe +" img").attr("src", selectedimage);
+    });
 });

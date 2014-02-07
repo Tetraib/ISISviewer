73798a6b5767529500eb662674ac696884023724
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2014-02-07 05:00:02 -0500

    parse xml with cheerio

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require("express"),
     app = express();
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser({
-    mergeAttrs: true,
-    explicitArray: false
-});
 var fs = require('fs');
+var cheerio = require('cheerio');
 app.configure(function() {
     app.use(express.static(__dirname + '/public'));
     app.use(express.json());
@@ -13,29 +9,62 @@ app.configure(function() {
     app.use(express.multipart());
 });
 app.listen(process.env.PORT, process.env.IP);
+//prototype
+function Dcm2json(xmldata, image) {
+    this.image = image;
+    this.dcmobject = {};
+    this.$ = cheerio.load(xmldata, {
+        normalizeWhitespace: true,
+        xmlMode: true
+    });
+}
+Dcm2json.prototype = {
+    constructor: Dcm2json,
+    get jsondata() {
+        this.$('element').each(function(i, elem) {
+            if ($(this).text() !== "") {
+                this.dcmobject[$(this).attr("name")] = [{
+                    "val": $(this).text()
+                }, {
+                    "tag": $(this).attr("tag")
+                }, {
+                    "vr": $(this).attr("vr")
+                }];
+            }
+        });
+        return this.dcmobject;
+    }
+};
+
+
 app.get('/', function(req, res) {
     res.render("home.jade");
 });
 
-app.post('/test', function(req, res) {
-    var convertedobj = {};
+app.post('/dicomin', function(req, res) {
+    var dicom = {};
     // curl command :
-    // curl "https://isisviewer-c9-tetraib.c9.io/test" -F image=@"B2.jpg" -F data=@"B2.xml"
+    // curl "https://isisviewer-c9-tetraib.c9.io/dicomin" -F image=@"B2.jpg" -F data=@"B2.xml"
     fs.readFile(req.files.data.path, 'utf8', function(err, data) {
         if (err) {
             return console.log(err);
         }
-        parser.parseString(data, function(err, result) {
-            for (var i in result.test.element) {
-                convertedobj[result.test.element[i].tag] = [{
-                    "value": result.test.element[i]._
-                }, {
-                    "name": result.test.element[i].name
-                }];
-
-            }
-            console.log(convertedobj);
+        var $ = cheerio.load(data, {
+            normalizeWhitespace: true,
+            xmlMode: true
         });
+        $('element').each(function(i, elem) {
+            if ($(this).text() !== "") {
+                dicom[$(this).attr("name")] = [{
+                    "val": $(this).text()
+                }, {
+                    "tag": $(this).attr("tag")
+                }, {
+                    "vr": $(this).attr("vr")
+                }];
+            }
+        });
+        console.log(dicom);
     });
     res.send(200);
 });
